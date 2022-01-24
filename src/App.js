8cef3e43ef1e7d527360bc497c79e16d067c2f18
Alexis <sixela295@gmail.com>
2022-01-23 22:32:03 -0500

    SignIn/Up page created, SignIn component created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import HomePage from './components/pages/homepage/homepage.component.jsx';
 import ShopPage from './components/pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
-
+import SignInAndUpPage from './components/pages/sign-in-and-up/sign-in-and-up.component.jsx';
 import { Switch, Route } from 'react-router-dom';
 import './App.scss';
 
@@ -13,7 +13,8 @@ function App() {
       <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />    
-        <Route path='/shop' component={ShopPage} />         
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndUpPage} />         
       </Switch>
     </div>
   );
---
+++ src/components/pages/sign-in-and-up/sign-in-and-up.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './sign-in-and-up.styles.scss';
+
+const SignInAndUpPage = () => (
+  <div className='sign-in-and-sign-up'>
+    SIGN IN
+  </div>
+);
+
+export default SignInAndUpPage;
